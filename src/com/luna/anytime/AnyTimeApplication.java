ac4e6fd1ad53f2e33025b935e70dcb235a40285e
LAN <tian.lan@capgemini.com>
2016-04-15 17:36:15 +0200

    update gradle

--- src/com/luna/anytime/AnyTimeApplication.java
+++ src/com/luna/anytime/AnyTimeApplication.java
@@ -8,12 +8,11 @@ public class AnyTimeApplication extends Application {
 
 	@Override
 	public void onCreate() {
-		// TODO Auto-generated method stub
 		super.onCreate();
 		AVOSCloud.useAVCloudCN();
 		// U need your AVOS key and so on to run the code.
 		AVOSCloud.initialize(this,
-				"70l90kzm53nplnu013ala0j8wipr594d36m5zuz94ukvmh5s",
-				"lamrsuheyiaqcx4o7m3jaz4awaeukerit1mucnjwk7ibokfv");
+				"K3Ti4Lyb5nbKKCeMesma0D6b-gzGzoHsz",
+				"pXOpgMYwjqtAG4Qooe3GqTuB");
 	}
 }
